1e3ae2066ef8ee304d4d4a83d78528bc6fa2c5a2
CLV\thong.le <thongle.clv@gmail.com>
2023-12-12 14:53:18 +0700

    auto installation

---
+++ autoinstallation/pages/index.tsx
@@ -0,0 +1,36 @@
+import React, { useState, FormEvent } from 'react'
+ 
+export default function Page() {
+  const [isLoading, setIsLoading] = useState<boolean>(false)
+ 
+  async function onSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+    setIsLoading(true) // Set loading to true when the request starts
+ 
+    try {
+      const formData = new FormData(event.currentTarget)
+      const response = await fetch('/api/submit', {
+        method: 'POST',
+        body: formData,
+      })
+ 
+      // Handle response if necessary
+      const data = await response.json()
+      // ...
+    } catch (error) {
+      // Handle error if necessary
+      console.error(error)
+    } finally {
+      setIsLoading(false) // Set loading to false when the request completes
+    }
+  }
+ 
+  return (
+    <form onSubmit={onSubmit}>
+      <input type="text" name="name" />
+      <button type="submit" disabled={isLoading}>
+        {isLoading ? 'Loading...' : 'Submit'}
+      </button>
+    </form>
+  )
+}
