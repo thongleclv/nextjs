13853b0f3026e1289e8ffe9662c7ee361e00e3c4
CLV\thong.le <thongle.clv@gmail.com>
2023-12-15 17:25:37 +0700

    complete blog

--- blog/pages/_app.js
+++ blog/pages/_app.js
@@ -1,5 +1,17 @@
-import '@/styles/globals.css'
+import Head from 'next/head';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import '../styles/globals.css';
+import Layout from '../components/layout/layout';
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <Layout>
+      <Head>
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+      </Head>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
+
+export default MyApp;
